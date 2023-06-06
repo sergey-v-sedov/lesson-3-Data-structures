ea4041a10cbe1e41156e3477c9937be02981bada
sergey-v-sedov <sergey.v.sedov@gmail.com>
2023-06-06 21:19:43 +0300

    Next commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +8,18 @@ public class Main {
         List<Document> docs = new ArrayList<>();
         docs.add(new Document(1, "Счет 1"));
         docs.add(new Document(2, "Счет 2"));
+        docs.add(new Document(3, "Счет 3"));
+
+        docs.add(1, new Document(4, "Счет 4"));
+
+        Document doc5 = new Document(5, "Счет 5");
+        docs.set(2, doc5);
+
+        docs.remove(0);
+        //docs.remove(doc5);
+
+        int doc5Indx = docs.indexOf(doc5);
+        System.out.println(doc5Indx);
 
         printDocuments(docs);
     }
